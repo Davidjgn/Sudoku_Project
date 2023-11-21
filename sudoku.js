64d96abb2386e32bc0d3a6baec3653fe8a47de1c
JeffWex <37007023+JeffWex@users.noreply.github.com>
2023-11-21 15:12:35 -0800

    Add files via upload

--- sudoku.js
+++ sudoku.js
@@ -1,27 +1,16 @@
-
+    
 let numSelected = null;
 let tileSelected = null;
 let loadDataInfo = null
 var errors = 0;
-const board = Array.from({ length: 9 }, () => Array.from({ length: 9 }, () => 0)); // creating 9 arrays which have 9 values [0,0,0,...] * 9
-
-let solution = [
-    "387491625",
-    "241568379",
-    "569327418",
-    "758619234",
-    "123784596",
-    "496253187",
-    "934176852",
-    "675832941",
-    "812945763"
-]
+let board = Array.from({ length: 9 }, () => Array.from({ length: 9 }, () => 0));
 
 const MockData = (data) => { // Funcion to load information from Json
-    //debugger;
+    debugger;
     loadDataInfo = JSON.parse(data);
-    setGame(loadDataInfo); // callback function to create digits number and set numbers.
+    createSudokuBoard(loadDataInfo); // callback function to create digits number and set numbers.
 
+    ValidationBoard(board);
 }
 
 const errorFun = (msg) => {
@@ -39,87 +28,70 @@ let loadPromise = new Promise((load, error) => {
     httpReq.send();
 }).then(MockData, errorFun);
 
-
-function setGame(loadDataInfo) {
-    // Digits 1-9
-    for (let i = 1; i <= 9; i++) {
-        let number = document.createElement("div");
-        number.id = i
-        number.innerText = i;
-        number.addEventListener("click", selectNumber);
-        number.classList.add("number");
-        document.getElementById("digits").appendChild(number);
-    };
-
-    // ------------------------ Remember 232234327 for 2 is column 3 is row and 2 is values, if have in 2 and 3 need be replace 4 like that example. --//
-    
-   //debugger;
-    createSudokuBoard(loadDataInfo);
-}
-
 function createSudokuBoard(data) {
-    //debugger;
-    
+    debugger;
 
-    for(let i = 0; i < data.length; i++){  // data[i] = each array e.g.[232234327]
-        if(i == 0){
-            const [col1, row1, val1, col2, row2, val2, col3, row3, val3] = data[i].val.split('').map(Number); // change individual string to number and return it
+    console.log(board);
+
+    for (let i = 0; i < data.length; i++) {
+        if (i == 0) {
+            const [col1, row1, val1, col2, row2, val2, col3, row3, val3] = data[i].val.split('').map(Number);
             board[row1 - 1][col1 - 1] = val1;
             board[row2 - 1][col2 - 1] = val2;
             board[row3 - 1][col3 - 1] = val3;
         }
-        else if(i == 1){
+        else if (i == 1) {
             const [col1, row1, val1, col2, row2, val2, col3, row3, val3] = data[i].val.split('').map(Number);
             board[row1 - 1][col1 + 2] = val1;
             board[row2 - 1][col2 + 2] = val2;
             board[row3 - 1][col3 + 2] = val3;
         }
-        else if(i == 2){
+        else if (i == 2) {
             const [col1, row1, val1, col2, row2, val2, col3, row3, val3] = data[i].val.split('').map(Number);
             board[row1 - 1][col1 + 5] = val1;
             board[row2 - 1][col2 + 5] = val2;
             board[row3 - 1][col3 + 5] = val3;
         }
-        else if(i == 3){
+        else if (i == 3) {
             const [col1, row1, val1, col2, row2, val2, col3, row3, val3] = data[i].val.split('').map(Number);
             board[row1 + 2][col1 - 1] = val1;
             board[row2 + 2][col2 - 1] = val2;
             board[row3 + 2][col3 - 1] = val3;
         }
-        else if(i == 4){
+        else if (i == 4) {
             const [col1, row1, val1, col2, row2, val2, col3, row3, val3] = data[i].val.split('').map(Number);
             board[row1 + 2][col1 + 2] = val1;
             board[row2 + 2][col2 + 2] = val2;
             board[row3 + 2][col3 + 2] = val3;
         }
-        else if(i == 5){
+        else if (i == 5) {
             const [col1, row1, val1, col2, row2, val2, col3, row3, val3] = data[i].val.split('').map(Number);
             board[row1 + 2][col1 + 5] = val1;
             board[row2 + 2][col2 + 5] = val2;
             board[row3 + 2][col3 + 5] = val3;
         }
-        else if(i == 6){
+        else if (i == 6) {
             const [col1, row1, val1, col2, row2, val2, col3, row3, val3] = data[i].val.split('').map(Number);
             board[row1 + 5][col1 - 1] = val1;
             board[row2 + 5][col2 - 1] = val2;
             board[row3 + 5][col3 - 1] = val3;
         }
-        else if(i == 7){
+        else if (i == 7) {
             const [col1, row1, val1, col2, row2, val2, col3, row3, val3] = data[i].val.split('').map(Number);
             board[row1 + 5][col1 + 2] = val1;
             board[row2 + 5][col2 + 2] = val2;
             board[row3 + 5][col3 + 2] = val3;
         }
-        else if(i == 8){
+        else if (i == 8) {
             const [col1, row1, val1, col2, row2, val2, col3, row3, val3] = data[i].val.split('').map(Number);
             board[row1 + 5][col1 + 5] = val1;
             board[row2 + 5][col2 + 5] = val2;
             board[row3 + 5][col3 + 5] = val3;
         }
     }
-    
-    // Board 9x9
-    for (let r = 0; r < 9; r++) {   // row loop 
+
+     // Board 9x9
+     for (let r = 0; r < 9; r++) {   // row loop 
         //debugger;
         for (let c = 0; c < 9; c++) {  //column loop
             //let tile = document.createElement("div");
@@ -144,15 +116,17 @@ function createSudokuBoard(data) {
             document.getElementById("board").append(tile);
         }
     }
-  }
+
+}
 
 const validInput = (event)=>{
     let flag = false;
-    console.log( event.target.value);
     for(let i = 1; i<10;i++){
         if(event.target.value == i || event.target.value == ''){
             flag = true;
             if(event.target.value != ''){
+                event.target.classList.remove("empty");
+                event.target.classList.remove("test");
                 const [r, c] = event.target.id.split('-').map(Number);
                 board[r][c]= Number(event.target.value);
             }
@@ -162,34 +136,74 @@ const validInput = (event)=>{
     if(flag == false){
         alert("Please input only one digit from 1-9")
         event.target.value = '';
+        event.target.classList.add("empty");
     }
 }
 
-function selectNumber() {
-    if (numSelected != null) {
-        numSelected.classList.remove("number-selected");
-    }
-    numSelected = this;
-    numSelected.classList.add("number-selected");
+function ValidationBoard(board) {
+
+    let btnValid = document.querySelector("button");
+    btnValid.innerText = "Validation";
+    btnValid.type = "button";
+
+    btnValid.addEventListener("click", () => {
+        checkDuplicates(board);
+    })
 }
 
-function selectTile() {
-    if (numSelected) {
-        if (this.innerText != "") {
-            return;
-        }
-
-        // "0-0" "0-1" .. "3-1"
-        let coords = this.id.split("-"); //["0", "0"]
-        let r = parseInt(coords[0]);
-        let c = parseInt(coords[1]);
-
-        if (solution[r][c] == numSelected.id) {
-            this.innerText = numSelected.id;
-        }
-        else {
-            errors += 1;
-            document.getElementById("errors").innerText = errors;
+function checkDuplicates(board) {
+    debugger;
+    // Check rows
+    for (let i = 0; i < board.length; i++) {
+        let rowMap = {};
+        for (let j = 0; j < board[i].length; j++) {
+            if (board[i][j] !== 0) {
+                if (rowMap[board[i][j]]) {
+                    debugger;
+                    alert("Duplicate elements exist");
+                    return true; // Duplicate found in row
+                }
+                rowMap[board[i][j]] = true;
+            }
+            else{
+                alert("You need complete all squares");
+            }
         }
     }
+
+    // Check columns
+    for (let j = 0; j < board[0].length; j++) {
+        let colMap = {};
+        for (let i = 0; i < board.length; i++) {
+            if (board[i][j] !== 0) {
+                if (colMap[board[i][j]]) {
+                    debugger;
+                    alert("Duplicate elements exist");
+                    return true; // Duplicate found in column
+                }
+                colMap[board[i][j]] = true;
+            }
+        }
+    }
+
+    // Check 3x3 subgrids
+    for (let row = 0; row < 9; row += 3) {
+        for (let col = 0; col < 9; col += 3) {
+            let subgridMap = {};
+            for (let i = row; i < row + 3; i++) {
+                for (let j = col; j < col + 3; j++) {
+                    if (board[i][j] !== 0) {
+                        if (subgridMap[board[i][j]]) {
+                            debugger;
+                            alert("Duplicate elements exist");
+                            return true; // Duplicate found in subgrid
+                        }
+                        subgridMap[board[i][j]] = true;
+                    }
+                }
+            }
+        }
+    }
+    alert("You Won!!")
+    return false; // No duplicates found
 }
