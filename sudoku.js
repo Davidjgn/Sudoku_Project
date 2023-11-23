a0a9fea0d3aef215309e2ea253d37aa39ffa25e2
junehorie <otoha202307@gmail.com>
2023-11-23 01:29:58 -0800

    comments added

--- sudoku.js
+++ sudoku.js
@@ -4,8 +4,6 @@ let errors = 0;  // = counter variable
 
 // generating 9 arrays which include 9 arrays inside, 9 * 9 matrix
 let board = Array.from({ length: 9 }, () => Array.from({ length: 9 }, () => 0));
-console.log(board);
-
 
 // STEP2 MockData function called with JSON data (parameter coming from promise)
 const MockData = (data) => {
@@ -35,31 +33,29 @@ let loadPromise = new Promise((load, error) => {
 
 
 // STEP3 9*9 board created
-function createSudokuBoard(data) {   // [{} {} {} {} {} {} {} {} {}] 
+function createSudokuBoard(data) {
     // debugger;
 
-    // console.log(data, board);   // data.length = 9 objects, each object looks like {val: '232234327'} 
     for (let i = 0; i < data.length; i++) {
 
         if (i == 0) {  // if data[i] = 0, meaning first object {val: '232234327'}
-            console.log(data[i].val.split('').map(Number))
             // dividing '232234327' into '2' '3' '2' '2' '3' '4' '3' '2' '7'
             // storing each string into an array, changing them from string to number =>[2, 3, 2, 2, 3, 4, 3, 2, 7]
             // then, assigning a variable to each number
             const [col1, row1, val1, col2, row2, val2, col3, row3, val3] = data[i].val.split('').map(Number);
 
-
             // identifying each board address by converting its row1 and col1 to index 
-            // then, assigning val1, val2, val3 to each board;
-            board[row1 - 1][col1 - 1] = val1;  // board[0][0] → board[3-1][2-1] →board[2][1] = 2
-            board[row2 - 1][col2 - 1] = val2;  // board[0][0] → board[3-1][2-1] →board[2][1] = 4
-            board[row3 - 1][col3 - 1] = val3;  // board[0][0] → board[2-1][3-1] →board[1][2] = 7
+            // then, assigning val1, val2, val3 to each 3*3 square;
+            board[row1 - 1][col1 - 1] = val1;  
+            board[row2 - 1][col2 - 1] = val2;  
+            board[row3 - 1][col3 - 1] = val3; 
         }
         else if (i == 1) {
+            // '113214227' => [1, 1, 3, 2, 1, 4, 2, 2, 7]
             const [col1, row1, val1, col2, row2, val2, col3, row3, val3] = data[i].val.split('').map(Number);
-            board[row1 - 1][col1 + 2] = val1;
-            board[row2 - 1][col2 + 2] = val2;
-            board[row3 - 1][col3 + 2] = val3;
+            board[row1 - 1][col1 + 2] = val1; 
+            board[row2 - 1][col2 + 2] = val2; 
+            board[row3 - 1][col3 + 2] = val3; 
         }
         else if (i == 2) {
             const [col1, row1, val1, col2, row2, val2, col3, row3, val3] = data[i].val.split('').map(Number);
@@ -105,27 +101,26 @@ function createSudokuBoard(data) {   // [{} {} {} {} {} {} {} {} {}]
         }
     }
 
-    // Board 9x9
+    // creating each tile for 9*9 matrix
     for (let r = 0; r < 9; r++) {   // row loop 
         //debugger;
         for (let c = 0; c < 9; c++) {  //column loop
             //let tile = document.createElement("div");
             let tile = document.createElement("input");
             tile.type = "text";
-            tile.id = r.toString() + "-" + c.toString();  // assign each box row&column id  0-0
-            if (board[r][c] != "0") {
-                //tile.innerText = board[r][c];
+            tile.id = r.toString() + "-" + c.toString();  // assigning each box row&column id e.g.) 0-0
+            if (board[r][c] != "0") {  // if the value is not 0, disable click event and change bgc
                 tile.value = board[r][c];
                 tile.setAttribute("disabled", true);
                 tile.classList.add("tile-start");
             }
-            if (r == 2 || r == 5) {
-                tile.classList.add("horizontal-line");  // add horizontal line 
+            if (r == 2 || r == 5) {  // adding horizontal line every 3 tiles
+                tile.classList.add("horizontal-line");  
             }
-            if (c == 2 || c == 5) {  // add vertical line 
+            if (c == 2 || c == 5) {  // adding vertical line every 3 tiles
                 tile.classList.add("vertical-line");
             }
-            tile.addEventListener("change", validInput);
+            tile.addEventListener("change", validInput);  // calling input tag's change event
             tile.classList.add("tile");
 
             document.getElementById("board").append(tile);
